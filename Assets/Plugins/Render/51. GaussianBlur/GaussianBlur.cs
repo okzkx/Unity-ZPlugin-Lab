7e78d00d69182e72a02dceb996bdb1dc887e71fa
okzkx <okzkx@qq.com>
2024-08-28 23:02:53 +0800

    Fix error

--- Assets/Plugins/Render/51. GaussianBlur/GaussianBlur.cs
+++ Assets/Plugins/Render/51. GaussianBlur/GaussianBlur.cs
@@ -1,44 +1,92 @@
+// using UnityEngine;
+// using UnityEngine.Rendering.HighDefinition;
+// using UnityEngine.Rendering;
+// using UnityEngine.Experimental.Rendering;
+// using UnityEngine.Rendering.Universal;
+//
+// public class GaussianBlur : ScriptableRenderPass {
+//     
+//     // Specifies the radius for the blur in pixels. This example uses an 8 pixel radius.
+//     public float blurFactor = 8.0f;
+//
+//     // Specifies the precision of the blur. This also affects the resource intensity of the blue. A value of 9 is good for real-time applications.
+//     const int sampleCount = 9;
+//
+//     RTHandle halfResTarget;
+//
+//     public override void Setup(ScriptableRenderContext context, ref RenderingData renderingData) {
+//         halfResTarget = RTHandles.Alloc(
+//             // Note the * 0.5f here. This allocates a half-resolution target, which saves a lot of memory.
+//             Vector2.one * 0.5f, TextureXR.slices, dimension: TextureXR.dimension,
+//             // Since alpha is unnecessary for Gaussian blur, this effect uses an HDR texture format with no alpha channel.
+//             colorFormat: GraphicsFormat.B10G11R11_UFloatPack32,
+//             // When creating textures, be sure to name them as it is useful for debugging.
+//             useDynamicScale: true, name: "Half Res Custom Pass"
+//         );
+//         
+//     }
+//     
+//     
+//     
+//     public override void Execute(ScriptableRenderContext ctx, ref RenderingData renderingData) {
+//         float radius = blurFactor;
+//     
+//         // In cases where you have multiple cameras with different resolutions, this makes the blur coherent across these cameras.
+//         radius *= ctx.cameraColorBuffer.rtHandleProperties.rtHandleScale.x;
+//     
+//         // The actual Gaussian blur call. It specifies the current camera's color buffer as the source and destination.
+//         // This uses the half-resolution target as a temporary render target between the blur passes.
+//         // Note that the Gaussian blur function clears the content of the half-resolution buffer when it finishes.
+//         CustomPassUtils.GaussianBlur(
+//             ctx, ctx.cameraColorBuffer, ctx.cameraColorBuffer, halfResTarget,
+//             sampleCount, radius, downSample: true
+//         );
+//     // }
+//
+//     // Releases the GPU memory allocated for the half-resolution target. This is important otherwise the memory will leak.
+//     protected override void Cleanup() => halfResTarget.Release();
+//
+//     public GaussianBlur(ScriptableRendererData data) : base(data) {
+//     }
+// }
+
+using System;
 using UnityEngine;
-using UnityEngine.Rendering.HighDefinition;
 using UnityEngine.Rendering;
-using UnityEngine.Experimental.Rendering;
+using UnityEngine.Rendering.Universal;
 
-public class GaussianBlur : CustomPass {
+[Serializable, VolumeComponentMenuForRenderPipeline("Post-processing/GaussianBlur", typeof(UniversalRenderPipeline))]
+public sealed partial class GaussianBlur : VolumeComponent, IPostProcessComponent {
+    /// <summary>
+    /// Controls the strength of the bloom filter.
+    /// </summary>
+    [Tooltip("Strength of the bloom filter.")]
+    public MinFloatParameter intensity = new MinFloatParameter(0f, 0f);
     
-    // Specifies the radius for the blur in pixels. This example uses an 8 pixel radius.
-    public float blurFactor = 8.0f;
+    public bool IsActive() => intensity.value > 0f;
 
-    // Specifies the precision of the blur. This also affects the resource intensity of the blue. A value of 9 is good for real-time applications.
-    const int sampleCount = 9;
+    /// <inheritdoc/>
+    public bool IsTileCompatible() => false;
+}
 
-    RTHandle halfResTarget;
+public class GaussianBlurPass : ScriptableRenderPass {
+    private GaussianBlur GaussianBlur;
 
-    protected override void Setup(ScriptableRenderContext renderContext, CommandBuffer cmd) {
-        halfResTarget = RTHandles.Alloc(
-            // Note the * 0.5f here. This allocates a half-resolution target, which saves a lot of memory.
-            Vector2.one * 0.5f, TextureXR.slices, dimension: TextureXR.dimension,
-            // Since alpha is unnecessary for Gaussian blur, this effect uses an HDR texture format with no alpha channel.
-            colorFormat: GraphicsFormat.B10G11R11_UFloatPack32,
-            // When creating textures, be sure to name them as it is useful for debugging.
-            useDynamicScale: true, name: "Half Res Custom Pass"
-        );
+    public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData) {
+        var stack = VolumeManager.instance.stack;
+        GaussianBlur = stack.GetComponent<GaussianBlur>();
+        
+    }
+}
+
+public class ZRenderFeature : ScriptableRendererFeature {
+    private GaussianBlurPass GaussianBlurPass;
+    
+    public override void Create() {
+        GaussianBlurPass = new GaussianBlurPass();
     }
 
-    protected override void Execute(CustomPassContext ctx) {
-        float radius = blurFactor;
-
-        // In cases where you have multiple cameras with different resolutions, this makes the blur coherent across these cameras.
-        radius *= ctx.cameraColorBuffer.rtHandleProperties.rtHandleScale.x;
-
-        // The actual Gaussian blur call. It specifies the current camera's color buffer as the source and destination.
-        // This uses the half-resolution target as a temporary render target between the blur passes.
-        // Note that the Gaussian blur function clears the content of the half-resolution buffer when it finishes.
-        CustomPassUtils.GaussianBlur(
-            ctx, ctx.cameraColorBuffer, ctx.cameraColorBuffer, halfResTarget,
-            sampleCount, radius, downSample: true
-        );
+    public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData) {
+        renderer.EnqueuePass(GaussianBlurPass);
     }
-
-    // Releases the GPU memory allocated for the half-resolution target. This is important otherwise the memory will leak.
-    protected override void Cleanup() => halfResTarget.Release();
 }
--- Assets/Plugins/Render/52. OverrideCamera/OverrideCamera.cs
+++ Assets/Plugins/Render/52. OverrideCamera/OverrideCamera.cs
@@ -1,72 +1,72 @@
-using UnityEngine;
-using UnityEngine.Rendering.HighDefinition;
-using UnityEngine.Rendering;
-
-public class OverrideCamera : CustomPass {
-    public float fov = 45;
-    public LayerMask overrideMask;
-    Camera overrideCamera;
-    const string kCameraTag = "OverrideCamera";
-    Material depthClearMaterial;
-    RTHandle depthBuffer;
-    public Vector3 position;
-    public Quaternion rotation;
-
-    protected override bool executeInSceneView => false;
-
-    protected override void AggregateCullingParameters(ref ScriptableCullingParameters cullingParameters, HDCamera hdCamera) {
-        cullingParameters.cullingMask |= (uint) overrideMask.value;
-    }
-
-    protected override void Setup(ScriptableRenderContext renderContext, CommandBuffer cmd) {
-        depthClearMaterial = new Material(Shader.Find("Hidden/Renderers/OverrideDepthClear"));
-        var dethBuffer = new RenderTargetIdentifier(BuiltinRenderTextureType.Depth);
-        depthBuffer = RTHandles.Alloc(dethBuffer);
-    }
-
-    protected override void Execute(CustomPassContext ctx) {
-        if (overrideCamera == null) {
-            CreateOverrideCamera();
-        }
-
-        overrideCamera.transform.SetPositionAndRotation(position, rotation);
-        overrideCamera.fieldOfView = fov;
-
-        // Use overrideCamera's cullingParameter to override CurrentCam's Culling result
-        if (overrideCamera.TryGetCullingParameters(out var cullingParameters)) {
-            cullingParameters.cullingOptions = CullingOptions.None;
-            ctx.cullingResults = ctx.renderContext.Cull(ref cullingParameters);
-        }
-
-        // Override depth to 0 (avoid artifacts with screen-space effects)
-        ctx.cmd.SetRenderTarget(depthBuffer);
-        CustomPassUtils.RenderFromCamera(ctx, overrideCamera, null, null,
-            ClearFlag.None, overrideMask, overrideMaterial: depthClearMaterial, overrideMaterialIndex: 0);
-        // Render the object color
-        CustomPassUtils.RenderFromCamera(ctx, overrideCamera, ctx.cameraColorBuffer, ctx.cameraDepthBuffer,
-            ClearFlag.None, overrideMask);
-    }
-
-    private void CreateOverrideCamera() {
-        // Hidden override camera:
-        var cam = GameObject.Find(kCameraTag);
-        if (cam == null) {
-            // cam = new GameObject(kCameraTag); // For Debugs
-            cam = new GameObject(kCameraTag) {hideFlags = HideFlags.HideAndDontSave};
-        }
-
-        if (!cam.TryGetComponent<Camera>(out var camera) || camera == null) {
-            camera = cam.AddComponent<Camera>();
-            camera.enabled = false;
-            camera.cullingMask = overrideMask;
-        }
-
-        overrideCamera = camera;
-    }
-
-    protected override void Cleanup() {
-        depthBuffer.Release();
-        CoreUtils.Destroy(depthClearMaterial);
-        CoreUtils.Destroy(overrideCamera.gameObject);
-    }
-}
+// using UnityEngine;
+// using UnityEngine.Rendering.HighDefinition;
+// using UnityEngine.Rendering;
+//
+// public class OverrideCamera : CustomPass {
+//     public float fov = 45;
+//     public LayerMask overrideMask;
+//     Camera overrideCamera;
+//     const string kCameraTag = "OverrideCamera";
+//     Material depthClearMaterial;
+//     RTHandle depthBuffer;
+//     public Vector3 position;
+//     public Quaternion rotation;
+//
+//     protected override bool executeInSceneView => false;
+//
+//     protected override void AggregateCullingParameters(ref ScriptableCullingParameters cullingParameters, HDCamera hdCamera) {
+//         cullingParameters.cullingMask |= (uint) overrideMask.value;
+//     }
+//
+//     protected override void Setup(ScriptableRenderContext renderContext, CommandBuffer cmd) {
+//         depthClearMaterial = new Material(Shader.Find("Hidden/Renderers/OverrideDepthClear"));
+//         var dethBuffer = new RenderTargetIdentifier(BuiltinRenderTextureType.Depth);
+//         depthBuffer = RTHandles.Alloc(dethBuffer);
+//     }
+//
+//     protected override void Execute(CustomPassContext ctx) {
+//         if (overrideCamera == null) {
+//             CreateOverrideCamera();
+//         }
+//
+//         overrideCamera.transform.SetPositionAndRotation(position, rotation);
+//         overrideCamera.fieldOfView = fov;
+//
+//         // Use overrideCamera's cullingParameter to override CurrentCam's Culling result
+//         if (overrideCamera.TryGetCullingParameters(out var cullingParameters)) {
+//             cullingParameters.cullingOptions = CullingOptions.None;
+//             ctx.cullingResults = ctx.renderContext.Cull(ref cullingParameters);
+//         }
+//
+//         // Override depth to 0 (avoid artifacts with screen-space effects)
+//         ctx.cmd.SetRenderTarget(depthBuffer);
+//         CustomPassUtils.RenderFromCamera(ctx, overrideCamera, null, null,
+//             ClearFlag.None, overrideMask, overrideMaterial: depthClearMaterial, overrideMaterialIndex: 0);
+//         // Render the object color
+//         CustomPassUtils.RenderFromCamera(ctx, overrideCamera, ctx.cameraColorBuffer, ctx.cameraDepthBuffer,
+//             ClearFlag.None, overrideMask);
+//     }
+//
+//     private void CreateOverrideCamera() {
+//         // Hidden override camera:
+//         var cam = GameObject.Find(kCameraTag);
+//         if (cam == null) {
+//             // cam = new GameObject(kCameraTag); // For Debugs
+//             cam = new GameObject(kCameraTag) {hideFlags = HideFlags.HideAndDontSave};
+//         }
+//
+//         if (!cam.TryGetComponent<Camera>(out var camera) || camera == null) {
+//             camera = cam.AddComponent<Camera>();
+//             camera.enabled = false;
+//             camera.cullingMask = overrideMask;
+//         }
+//
+//         overrideCamera = camera;
+//     }
+//
+//     protected override void Cleanup() {
+//         depthBuffer.Release();
+//         CoreUtils.Destroy(depthClearMaterial);
+//         CoreUtils.Destroy(overrideCamera.gameObject);
+//     }
+// }
