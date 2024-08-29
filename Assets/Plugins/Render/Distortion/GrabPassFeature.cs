15f3a8bec6cdd5201f0647dadcd788ac19c81ac0
okzkx <okzkx@qq.com>
2024-08-29 22:17:12 +0800

    Remove useless codes

--- Assets/Plugins/Render/Distortion/GrabPassFeature.cs
+++ Assets/Plugins/Render/Distortion/GrabPassFeature.cs
@@ -2,57 +2,89 @@ using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.Universal;
 
-public class GrabPassFeature : ScriptableRendererFeature
-{
-    class GrabPass : ScriptableRenderPass
-    {
-        static readonly string k_RenderTag = "GrabPass"; //可在framedebug中看渲染
-        RenderTargetIdentifier currentTarget;
-        RenderTargetHandle tempColorTarget;
-        string m_GrabPassName = "_GrabPassTexture"; //shader中的grabpass名字
+public class GrabPass : ScriptableRenderPass {
+    static readonly string k_RenderTag = "GrabPass"; //可在framedebug中看渲染
+    ScriptableRenderer renderer;
 
-        public GrabPass()
-        {
-            renderPassEvent = RenderPassEvent.AfterRenderingTransparents;
-            tempColorTarget.Init(m_GrabPassName);
-        }
+    RTHandle tempColorTarget;
 
-        public void SetUp(RenderTargetIdentifier currentTarget)
-        {
-            this.currentTarget = currentTarget;
-        }
+    // string m_GrabPassName = ""; //shader中的grabpass名字
+    int _GrabPassTexture = Shader.PropertyToID("_GrabPassTexture");
 
-        public override void OnCameraSetup(CommandBuffer cmd, ref RenderingData renderingData)
-        {
-        }
+    public GrabPass() {
+        renderPassEvent = RenderPassEvent.AfterRenderingTransparents;
+        // tempColorTarget = RTHandles.Alloc(_GrabPassTexture, "_GrabPassTexture");
+        //
+        // var descriptor = renderer.cameraColorTargetHandle.rt.descriptor;
+        // RenderingUtils.ReAllocateIfNeeded(ref tempColorTarget, descriptor, 
+        //     FilterMode.Bilinear, TextureWrapMode.Clamp, name: "_SSAO_OcclusionTexture0");
 
-        public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
-        {
-            var cmd = CommandBufferPool.Get(k_RenderTag);
-
-            cmd.GetTemporaryRT(tempColorTarget.id, Screen.width, Screen.height); //获取临时rt
-            cmd.SetGlobalTexture(m_GrabPassName, tempColorTarget.Identifier()); //设置给shader中
-            Blit(cmd, currentTarget, tempColorTarget.Identifier());
-
-            context.ExecuteCommandBuffer(cmd);
-            CommandBufferPool.Release(cmd);
-        }
-
-        public override void OnCameraCleanup(CommandBuffer cmd)
-        {
-        }
+        // tempColorTarget.Init(m_GrabPassName);
     }
 
-    private GrabPass m_ScriptablePass;
 
-    public override void Create()
-    {
-        m_ScriptablePass = new GrabPass();
+    public void SetUp(ScriptableRenderer renderer) {
+        this.renderer = renderer;
     }
 
-    public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
-    {
-        m_ScriptablePass.SetUp(renderer.cameraColorTarget);
-        renderer.EnqueuePass(m_ScriptablePass);
+    public override void OnCameraSetup(CommandBuffer cmd, ref RenderingData renderingData) {
+        RenderTextureDescriptor cameraTargetDescriptor = renderingData.cameraData.cameraTargetDescriptor;
+        RenderTextureDescriptor descriptor = cameraTargetDescriptor;
+        descriptor.msaaSamples = 1;
+        descriptor.depthBufferBits = 0;
+
+        var m_AOPassDescriptor = descriptor;
+
+        RenderingUtils.ReAllocateIfNeeded(ref tempColorTarget, m_AOPassDescriptor, FilterMode.Bilinear, TextureWrapMode.Clamp, name: "_SSAO_OcclusionTexture0");
+        Shader.SetGlobalTexture(_GrabPassTexture, tempColorTarget);
+    }
+
+    public override void OnCameraCleanup(CommandBuffer cmd) {
+        // cmd.ReleaseTemporaryRT(_GrabPassTexture);
+    }
+
+    public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData) {
+        // return;
+
+        var cmd = CommandBufferPool.Get(k_RenderTag);
+
+        // cmd.GetTemporaryRT(tempColorTarget.nameID, Screen.width, Screen.height); //获取临时rt
+        // cmd.SetGlobalTexture(m_GrabPassName, tempColorTarget.Identifier()); //设置给shader中
+        
+        
+        
+        // cmd.GetTemporaryRT(_GrabPassTexture, renderingData.cameraData.cameraTargetDescriptor);
+
+        // Blit(cmd, renderer.cameraColorTargetHandle, tempColorTarget);
+        Blitter.BlitCameraTexture(cmd, renderer.cameraColorTargetHandle, tempColorTarget);
+        // Blitter.BlitCameraTexture(cmd, renderer.cameraColorTargetHandle, tempColorTarget);
+        
+        context.ExecuteCommandBuffer(cmd);
+        CommandBufferPool.Release(cmd);
+    }
+
+    public void Cleanup() {
+        tempColorTarget.Release();
+    }
+}
+
+public class PostEffectPass : ScriptableRenderPass {
+    ShaderTagId PostEffect = new ShaderTagId("PostEffect");
+    
+    public PostEffectPass() {
+        renderPassEvent = RenderPassEvent.BeforeRenderingPostProcessing;
+    }
+
+    public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData) {
+        SortingCriteria sortingCriteria = SortingCriteria.CommonTransparent;
+        DrawingSettings drawingSettings = CreateDrawingSettings(PostEffect, ref renderingData, sortingCriteria);
+
+        // RenderQueueRange renderQueueRange = (renderQueueType == RenderQueueType.Transparent)
+        //     ? RenderQueueRange.transparent
+        //     : RenderQueueRange.opaque;
+        // m_FilteringSettings = new FilteringSettings(renderQueueRange, outlineLayer);
+        var m_FilteringSettings = new FilteringSettings(RenderQueueRange.all);
+        
+        context.DrawRenderers(renderingData.cullResults, ref drawingSettings, ref m_FilteringSettings);
     }
 }
