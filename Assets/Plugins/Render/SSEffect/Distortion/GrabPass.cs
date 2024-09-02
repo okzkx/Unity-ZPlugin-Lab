0853d6e3ac188cee27794b10fbdb7f457cffdb2b
okzkx <okzkx@qq.com>
2024-09-03 02:04:42 +0800

    PostEffectPass

--- Assets/Plugins/Render/SSEffect/Distortion/GrabPass.cs
+++ Assets/Plugins/Render/SSEffect/Distortion/GrabPass.cs
@@ -32,24 +32,3 @@ public class GrabPass : SSPass<Grab> {
         base.Release();
     }
 }
-
-public class PostEffectPass : ScriptableRenderPass {
-    ShaderTagId PostEffect = new ShaderTagId("PostEffect");
-
-    public PostEffectPass() {
-        renderPassEvent = RenderPassEvent.BeforeRenderingPostProcessing;
-    }
-
-    public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData) {
-        SortingCriteria sortingCriteria = SortingCriteria.CommonTransparent;
-        DrawingSettings drawingSettings = CreateDrawingSettings(PostEffect, ref renderingData, sortingCriteria);
-
-        // RenderQueueRange renderQueueRange = (renderQueueType == RenderQueueType.Transparent)
-        //     ? RenderQueueRange.transparent
-        //     : RenderQueueRange.opaque;
-        // m_FilteringSettings = new FilteringSettings(renderQueueRange, outlineLayer);
-        var m_FilteringSettings = new FilteringSettings(RenderQueueRange.all);
-
-        context.DrawRenderers(renderingData.cullResults, ref drawingSettings, ref m_FilteringSettings);
-    }
-}
